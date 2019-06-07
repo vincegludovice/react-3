d46f3eceefcd3d2ad80b8ff1533f00799823f879
Aodhan Hayter <aodhan.hayter@gmail.com>
2019-06-07 15:29:46 -0600

    Add component files

---
+++ src/components/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+import Header from './Header/Header';
+import Compose from './Compose/Compose';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      posts: [],
+    };
+
+    this.updatePost = this.updatePost.bind(this);
+    this.deletePost = this.deletePost.bind(this);
+    this.createPost = this.createPost.bind(this);
+  }
+
+  componentDidMount() {}
+
+  updatePost() {}
+
+  deletePost() {}
+
+  createPost() {}
+
+  render() {
+    const { posts } = this.state;
+
+    return (
+      <div className="App__parent">
+        <Header />
+
+        <section className="App__content">
+          <Compose />
+        </section>
+      </div>
+    );
+  }
+}
+
+export default App;
